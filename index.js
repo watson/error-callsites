0626bfe51d87c080124bfe544df7dcab7a16cd54
Thomas Watson Steen <w@tson.dk>
2016-10-27 16:26:03 +0200

    fix: allow __error_callsites to be re-defined
    In Node.js v7 this used to throw when using this module because it emits
    a deprecation warning when trying to re-define the __error_callsites
    property. By defining that property as configurable this error goes
    away.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var orig = Error.prepareStackTrace
 Error.prepareStackTrace = function (err, callsites) {
   Object.defineProperty(err, '__error_callsites', {
     enumerable: false,
-    configurable: false,
+    configurable: true,
     writable: false,
     value: callsites
   })
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var util = require('util')
 var test = require('tape')
 var callsites = require('./')
 
@@ -34,5 +35,13 @@ test('process same error twice', function (t) {
   callsites(err)
   t.equal(typeof err.stack, 'string')
   t.ok(Array.isArray(err.__error_callsites))
+  t.end()
+})
+
+// In Node.js v7 this used to throw when using this module because it emits a
+// deprecation warning when trying to re-define the __error_callsites property.
+// By defining that property as configurable this error goes away.
+test('run deprecated function', function (t) {
+  util.deprecate(function () {}, 'foo')()
   t.end()
 })
