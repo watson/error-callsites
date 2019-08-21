bf339d28c29662e599bc1df189bcc5a0edede62d
Thomas Watson <w@tson.dk>
2019-08-21 17:16:33 +0200

    feat: support overwriting of Error.prepareStacktrace
    Previously, if the `Error.prepareStacktrace` function was overwritten
    after this module was loaded, we would loose the ability to set the
    magic `__error_callsites` property.
    
    With this commit, we'll always ensure to run our `prepareStacktrace`
    function before any other function added later.

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 'use strict'
 
-var formatter = require('./lib/node-0.10-formatter')
+const formatter = require('./lib/node-0.10-formatter')
 
-var orig = Error.prepareStackTrace
-Error.prepareStackTrace = function (err, callsites) {
+let lastPrepareStackTrace = Error.prepareStackTrace
+
+Object.defineProperty(Error, 'prepareStackTrace', {
+  configurable: true,
+  enumerable: true,
+  get: function () {
+    return prepareStackTrace
+  },
+  set: function (fn) {
+    lastPrepareStackTrace = fn
+  }
+})
+
+module.exports = function (err) {
+  err.stack
+  return err.__error_callsites
+}
+
+function prepareStackTrace (err, callsites) {
   Object.defineProperty(err, '__error_callsites', {
     enumerable: false,
     configurable: true,
     writable: false,
     value: callsites
   })
-
-  return (orig || formatter)(err, callsites)
-}
-
-module.exports = function (err) {
-  err.stack
-  return err.__error_callsites
+  return (lastPrepareStackTrace || formatter)(err, callsites)
 }
